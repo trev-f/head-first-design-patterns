229f8c21fc417e40047098465b685e2284f9bee7
Trevor Freeman <trvrfreeman@gmail.com>
2023-02-01 20:22:03 +0000

    Add a current conditions display

---
+++ weather_station/current_conditions_display.py
@@ -0,0 +1,29 @@
+from display_element import DisplayElement
+
+from observer import Observer
+
+from weather_data import WeatherData
+
+
+class CurrentConditionsDisplay(DisplayElement, Observer):
+
+    def __init__(self, weather_data: WeatherData) -> None:
+        self._temperature = weather_data.temperature
+        self._humidity = weather_data.humidity
+        self._weather_data = weather_data
+
+        weather_data.register_observer(self)
+    
+
+    def update(self, temp: float, humidity: float, pressure: float) -> None:
+        # update state
+        self._temperature = temp
+        self._humidity = humidity
+        _ = pressure
+
+        # update the display
+        self.display()
+    
+
+    def display(self) -> None:
+        print(f"Current conditions: {self._temperature} F degrees and {self._humidity}% humidity")
