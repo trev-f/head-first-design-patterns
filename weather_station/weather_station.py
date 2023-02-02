da4648e3ddd4b3743a4250ac74e7616e2de7e31e
Trevor Freeman <trvrfreeman@gmail.com>
2023-02-02 17:47:51 +0000

    Add temperature statistics display

--- weather_station/weather_station.py
+++ weather_station/weather_station.py
@@ -1,5 +1,6 @@
 from current_conditions_display import CurrentConditionsDisplay
 from forecast_display import ForecastDisplay
+from statistics_display import StatisticsDisplay
 from weather_data import WeatherData
 
 
@@ -10,6 +11,7 @@ class WeatherStation:
         weather_data = WeatherData()
 
         current_display = CurrentConditionsDisplay(weather_data)
+        statistics_display = StatisticsDisplay(weather_data)
         forecast_display = ForecastDisplay(weather_data)
 
         weather_data.set_measurements(temperature=80, humidity=65, pressure=30.4)
